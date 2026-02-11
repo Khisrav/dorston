e8910d9e3ffb7b2ee49bcddc901954237436db5e
Khisrav <kh.khisrav2018@gmail.com>
2026-02-12 02:29:55 +0300

    Refactor useComfortConstructive to streamline price calculation logic for door panels, consolidating utility function imports and enhancing readability. Introduce a new base price calculation method with improved handling of MDF panel costs and difficulty pricing based on door model. Update the calculation for film color pricing to ensure accurate total pricing in the configurator.

--- resources/js/composables/door-constructives/useComfortConstructive.ts
+++ resources/js/composables/door-constructives/useComfortConstructive.ts
@@ -1,10 +1,5 @@
 import { getComfortTotalSum } from '@/constants/comfort';
-import {
-    getPaintPrice,
-    isDoorPanelStandard,
-    isDoorStandard,
-    isMetallicDoor,
-} from '@/lib/utils';
+import { getPaintPrice, isDoorPanelStandard, isDoorStandard, isMetallicDoor } from '@/lib/utils';
 import { DoorConfig } from '@/types/configurator';
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
@@ -27,181 +22,281 @@ export const useComfortConstructive = defineStore('comfortConstructive', () => {
         //Закрыть короб
         total_price.value += closedBoxPriceValue(doorConfig);
         //Дополнительно крепеж если ПП
-        total_price.value += isMetallicDoor(doorConfig)
-            ? 0
-            : 100 * 0.18 + 4 * 1.6;
+        total_price.value += isMetallicDoor(doorConfig) ? 0 : 100 * 0.18 + 4 * 1.6;
         //Замок
         total_price.value += locksPriceValue(doorConfig, doorCalcStore);
         //Фурнитура
         total_price.value += furniturePriceValue(doorConfig, doorCalcStore);
         //Наружная МДФ-панель
-        total_price.value += exteriorPanelsPriceValue(
-            doorConfig,
-            doorCalcStore,
-        );
+        total_price.value += exteriorPanelsPriceValue(doorConfig, doorCalcStore);
+        //Внутренняя МДФ-панель
+        total_price.value += interiorPanelsPriceValue(doorConfig, doorCalcStore);
 
         return total_price.value;
     };
 
     //расчет наружных панелей
-    const exteriorPanelsPriceValue = (
-        doorConfig: DoorConfig,
-        doorCalcStore: ReturnType<typeof useDoorCalc>,
-    ) => {
-        const doorModel = doorCalcStore.doorModels?.find(
-            (m) => m.id === doorConfig.exterior.panelModel,
-        );
+    const exteriorPanelsPriceValue = ( doorConfig: DoorConfig, doorCalcStore: ReturnType<typeof useDoorCalc> ) => {
+        const doorModel = doorCalcStore.doorModels?.find((m) => m.id === doorConfig.exterior.panelModel);
         const modelName = doorModel?.name;
 
         //Модуль расчёта панелей на наружнюю отделку [Без вставки]
-        let basePriceValue = 0;
-        //МДФ t=10 мм, м2
-        basePriceValue +=
-            (isDoorPanelStandard(
-                doorConfig.doorWidth,
-                doorConfig.doorHeight,
-                true,
-            )
-                ? 1.932
-                : 2.89) * 272;
-        console.log(
-            isDoorPanelStandard(
-                doorConfig.doorWidth,
-                doorConfig.doorHeight,
-                true,
-            )
-                ? 1.932
-                : 2.89,
-            272,
-            basePriceValue,
-        );
-        //клей, Наждачная бумага, м2, Губка шлифовальная, Колер, Упаковка, накладные расходы, Зарплата панель, Заложенная сложность
-        basePriceValue +=
-            0.15 * 1000 +
-            0.005 * 214 +
-            0.05 * 60 +
-            0.018 * 55 +
-            1 * 30 +
-            1 * 734 +
-            1 * 65;
-        //заложенная сложность
-        const difficultyPrice = (modelName: string | undefined) => {
+        let basePriceValue = (includeMDF: boolean = true) => {
             let result = 0;
-            switch (modelName) {
-                case "ВС-19":
-                case "ВС-38":
-                case "ВС-6":
-                case "ВС-22":
-                    result = 800;
-                    break;
-                case "ВС-17":
-                case "ВС-13":
-                case "ВС-24":
-                case "ВС-45":
-                case "ВС-49":
-                case "ВС-50":
-                case "ВС-43":
-                case "ВС-52":
-                case "ВС-34":
-                    result = 1000;
-                    break;
-                case "ВС-44":
-                case "ВС-41":
-                case "ВС-47":
-                case "ВС-30":
-                    result = 600;
-                    break;
-                case "Stark":
-                case "Verso":
-                case "Forta":
-                case "Kombi":
-                default:
-                    result = 0;
-                    break;
+
+            //МДФ t=10 мм, м2
+            if (includeMDF) {
+                console.log('МДФ t=10 мм, м2', (isDoorPanelStandard( doorConfig.doorWidth, doorConfig.doorHeight, true ) ? 1.932 : 2.89) * 272);
+                result += (isDoorPanelStandard( doorConfig.doorWidth, doorConfig.doorHeight, true ) ? 1.932 : 2.89) * 272;
             }
+            //клей, Наждачная бумага, м2, Губка шлифовальная, Колер, Упаковка, накладные расходы, Зарплата панель, Заложенная сложность
+            result += 0.15 * 1000 + 0.005 * 214 + 0.05 * 60 + 0.018 * 55 + 1 * 30 + 1 * 734 + 1 * 65;
+            console.log('Мелочь', 0.15 * 1000 + 0.005 * 214 + 0.05 * 60 + 0.018 * 55 + 1 * 30 + 1 * 734 + 1 * 65);
+            //заложенная сложность
+            const difficultyPrice = (modelName: string | undefined) => {
+                let result = 0;
+                switch (modelName) {
+                    case "ВС-19":
+                    case "ВС-38":
+                    case "ВС-6":
+                    case "ВС-22":
+                        result = 800;
+                        break;
+                    case "ВС-17":
+                    case "ВС-13":
+                    case "ВС-24":
+                    case "ВС-45":
+                    case "ВС-49":
+                    case "ВС-50":
+                    case "ВС-43":
+                    case "ВС-52":
+                    case "ВС-34":
+                        result = 1000;
+                        break;
+                    case "ВС-44":
+                    case "ВС-41":
+                    case "ВС-47":
+                    case "ВС-30":
+                        result = 600;
+                        break;
+                    case "Stark":
+                    case "Verso":
+                    case "Forta":
+                    case "Kombi":
+                    default:
+                        result = 0;
+                        break;
+                }
+                return result;
+            };
+            result += difficultyPrice(modelName);
+            console.log('Заложенная сложность', difficultyPrice(modelName));
+            //цвет выбранной пленки
+            let filmColorPrice = doorCalcStore.filmColors?.find((f) => f.id === doorConfig.exterior.primaryTexture)?.base_price;
+            result += (isDoorPanelStandard( doorConfig.doorWidth, doorConfig.doorHeight, true, ) ? 3.5 : 4.55) * (filmColorPrice ?? 0);
+            console.log('Цвет выбранной пленки', (isDoorPanelStandard( doorConfig.doorWidth, doorConfig.doorHeight, true, ) ? 3.5 : 4.55) * (filmColorPrice ?? 0));
             return result;
         };
-        basePriceValue += difficultyPrice(modelName);
-        //цвет выбранной пленки
-        const filmColorPrice = doorCalcStore.filmColors?.find(
-            (f) => f.id === doorConfig.exterior.primaryTexture,
-        )?.base_price;
-        basePriceValue +=
-            (isDoorPanelStandard(
-                doorConfig.doorWidth,
-                doorConfig.doorHeight,
-                true,
-            )
-                ? 3.5
-                : 4.55) * (filmColorPrice ?? 0);
-        console.log(
-            isDoorPanelStandard(
-                doorConfig.doorWidth,
-                doorConfig.doorHeight,
-                true,
-            )
-                ? 3.5
-                : 4.55,
-            filmColorPrice,
-            basePriceValue,
-        );
 
-        let sum = basePriceValue;
+        let sum = 0
 
-        if (!isDoorStandard(doorConfig.doorWidth, doorConfig.doorHeight)) {
+        if (['ВС-6', 'ВС-13', 'ВС-17', 'ВС-19', 'ВС-22', 'ВС-24', 'ВС-38', 'ВС-44', 'ВС-45', 'Kombi', 'Forta', 'Verso', 'Stark'].includes(modelName ?? '') || modelName === undefined) {
+            const secondaryFilmColorPrice = doorCalcStore.filmColors?.find((f) => f.id === doorConfig.exterior.secondaryTexture)?.base_price;
             switch (modelName) {
-                case 'BC-6':
+                case "ВС-6":
+                    sum += basePriceValue(true);
+                    //МДФ t=6 мм, м2
+                    sum += 0.645 * 154.59
+                    console.log('МДФ t=6 мм, м2', 0.645 * 154.59);
+                    //Стекло
+                    sum += (isDoorPanelStandard(doorConfig.doorWidth, doorConfig.doorHeight) ? 0.0612 : 0.0669) * 1400
+                    console.log('Стекло', (isDoorPanelStandard(doorConfig.doorWidth, doorConfig.doorHeight) ? 0.0612 : 0.0669) * 1400);
+                    //Обработка
+                    sum += (isDoorPanelStandard(doorConfig.doorWidth, doorConfig.doorHeight) ? 4.14 : 4.52) * 60
+                    console.log('Обработка', (isDoorPanelStandard(doorConfig.doorWidth, doorConfig.doorHeight) ? 4.14 : 4.52) * 60);
+                    //Цвет выбранной доп пленки
+                    sum += (isDoorPanelStandard(doorConfig.doorWidth, doorConfig.doorHeight) ? 3.5 : 4.9) * (secondaryFilmColorPrice ?? 0);
+                    console.log('Цвет выбранной доп пленки', (isDoorPanelStandard(doorConfig.doorWidth, doorConfig.doorHeight) ? 3.5 : 4.9) * (secondaryFilmColorPrice ?? 0));
+                    //Клей 88, Скотч двухсторонний
+                    sum += 0.1 * 430 + 1 * 2.1
+                    console.log('Клей 88, Скотч двухсторонний', 0.1 * 430 + 1 * 2.1);
+                    break;
+                case "ВС-13":
+                    sum += basePriceValue(true);
+                    //МДФ t=6 мм, м2
+                    sum += 0.966 * 154.59
+                    console.log('МДФ t=6 мм, м2', 0.966 * 154.59);
+                    //зарплата [плюс к стандарту A182]
+                    sum += 1 * 25
+                    console.log('зарплата [плюс к стандарту A182]', 1 * 25);
+                    //Клей 88
+                    sum += 0.1 * 430
+                    break;
+                case "ВС-17":
                     sum += 0;
                     break;
-                case 'BC-13':
+                case "ВС-19":
                     sum += 0;
                     break;
-                case 'BC-17':
+                case "ВС-22":
                     sum += 0;
                     break;
-                case 'BC-19':
+                case "ВС-24":
                     sum += 0;
                     break;
-                case 'BC-22':
+                case "ВС-38":
                     sum += 0;
                     break;
-                case 'BC-24':
+                case "ВС-44":
                     sum += 0;
                     break;
-                case 'BC-38':
+                case "ВС-45":
                     sum += 0;
                     break;
-                case 'BC-44':
-                    sum += 0;
-                    break;
-                case 'BC-45':
+                default:
                     sum += 0;
                     break;
             }
 
             return sum;
         } else {
-            const qntTemp =
-                modelName === 'BC-6' ||
-                modelName === 'BC-13' ||
-                modelName === 'BC-17' ||
-                modelName === 'BC-19' ||
-                modelName === 'BC-22' ||
-                modelName === 'BC-24' ||
-                modelName === 'BC-38' ||
-                modelName === 'BC-44' ||
-                modelName === 'BC-45' ||
-                modelName === 'Kombi' ||
-                modelName === 'Forta' ||
-                modelName === 'Verso' ||
-                modelName === 'Stark' ||
-                modelName === undefined
-                    ? 0
-                    : 1;
-            sum += qntTemp * 0;
+            sum = basePriceValue(true);
             return sum;
         }
     };
+
+    //расчет внутренних панелей
+    const interiorPanelsPriceValue = (doorConfig: DoorConfig, doorCalcStore: ReturnType<typeof useDoorCalc>) => {
+        let sum = 0;
+        const isStandard = doorConfig.doorHeight > 2052 ? false : true;
+    
+        // МДФ t=10 мм, м2 - сюда же вшита 12мм мдф под панели с зеркалами, upd вшита 16мм
+        const modelName = doorCalcStore.doorModels?.find((m) => m.id === doorConfig.interior.panelModel)?.name;
+        const mdfPriceConditions = [
+            {
+                check: ['Ф-3', 'Ф-11', 'Ф-20', 'Ф-31', 'Ф-36', 'Ф-37', 'Ф-48', 'Ф-64', 'Ф-71', 'Ф-75', 'Ф-88', 'Ф-95', 'Ф-99',
+                        'Ф-21', 'Ф-22', 'Ф-23', 'Ф-26', 'Ф-42', 'Ф-24', 'Ф-30', 'Ф-32', 'Ф-76', 'Ф-79', 'ГЛ-1'],
+                price: 272 //'Управление'!.E64
+            },
+            {
+                check: ['ФЗ-1', 'ФЗ-6', 'ФЗ-7', 'ФЗ-8'],
+                price: 290.89 //'Управление'!.E78
+            },
+            {
+                check: ['Без панели'],
+                price: 0
+            },
+            {
+                check: ['Ф-62'],
+                price: 375 //'Управление'!.B159
+            }
+        ];
+        function getMdfPrice(value: string | undefined) {
+            if (!value) return 0
+            for (const condition of mdfPriceConditions) {
+                if (condition.check.includes(value)) return condition.price
+            }
+            return 0
+        }
+        const mdf_price = getMdfPrice(modelName);
+        sum += (isStandard ? 1.932 : 2.89) * mdf_price;
+        console.log('МДФ t=10 мм, м2', (isStandard ? 1.932 : 2.89) * mdf_price);
+
+        //Клей, Наждачная бумага, м2, Губка шлифовальная, Колер, Упаковка, накладные расходы, Зарплата панель
+        sum += 0.15 * 1000 + 0.005 * 214 + 0.05 * 60 + 0.018 * 55 + 1 * 20 + 1 * 734 + 1 * 65;
+
+        //Заложенная сложность
+        const difficultyPrice = (modelName: string | undefined) => {
+            let result = 0;
+            switch(modelName) {
+                case 'Ф-3':
+                case 'Ф-11':
+                case 'Ф-20':
+                case 'Ф-31':
+                case 'Ф-37':
+                case 'Ф-88':
+                case 'Ф-95':
+                case 'Ф-99':
+                case 'Ф-26':
+                case 'Ф-42':
+                case 'Ф-24':
+                case 'Ф-79':
+                    return 700;
+                case 'Ф-36':
+                case 'Ф-48':
+                case 'Ф-64':
+                case 'Ф-21':
+                case 'Ф-22':
+                case 'Ф-23':
+                case 'Ф-30':
+                case 'ГЛ-1':
+                case 'Ф-62':
+                    return 500;
+                case 'Ф-76':
+                case 'ФЗ-1':
+                case 'ФЗ-6':
+                case 'ФЗ-7':
+                case 'ФЗ-8':
+                    return 900;
+                default:
+                    return 0; // or a default value if needed
+            }
+        };
+        sum += 1 * difficultyPrice(modelName);
+
+        //Цвет выбранной пленки
+        const primaryFilmColorPrice = doorCalcStore.filmColors?.find((f) => f.id === doorConfig.interior.primaryTexture)?.base_price;
+        sum += (isStandard ? 3.5 : 4.55) * (primaryFilmColorPrice ?? 0);
+        console.log('Цвет выбранной пленки', (isStandard ? 3.5 : 4.55) * (primaryFilmColorPrice ?? 0));
+
+        //Стекло+обработка
+        const qntTemp = (modelName: string | undefined) => {
+            switch(modelName) {
+                case 'Ф-24':
+                case 'Ф-30':
+                    return 2
+                case 'Ф-32':
+                    return 1.5
+                case 'Ф-79':
+                    return 1
+                default:
+                    return 0
+            }
+        }
+        sum += qntTemp(modelName) * (isStandard ? 315 : 366)
+        console.log('Стекло+обработка', qntTemp(modelName) * (isStandard ? 315 : 366))
+
+        //Клей 88
+        const glueConsumption = (modelName: string | undefined) => {
+            switch(modelName) {
+                case 'Ф-24':
+                case 'Ф-30':
+                    return 0.1;
+                case 'Ф-32':
+                    return 0.15;
+                case 'Ф-79':
+                    return 0.05;
+                case 'ФЗ-1':
+                case 'ФЗ-6':
+                case 'ФЗ-7':
+                case 'ФЗ-8':
+                    return 0.4;
+                default:
+                    return 0;
+            }
+        }
+        sum += glueConsumption(modelName) * 430
+        console.log('Клей 88', glueConsumption(modelName) * 430)
+
+        //Стекло Ф-30
+        if (modelName === 'Ф-30') {
+            sum += (isStandard ? 0.12084 : 0.12864) * 1400
+            console.log('Стекло Ф-30', (isStandard ? 0.12084 : 0.12864) * 1400)
+        }
+
+        return sum;
+    };
+    
 
     const locksPriceValue = (
         doorConfig: DoorConfig,
